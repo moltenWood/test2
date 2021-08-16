70c9329f04dd5d8722773cfaed94b64a3800cf64
wudongze <898797402@qq.com>
2021-08-16 16:15:42 +0800

    a

---
+++ main.go
@@ -0,0 +1,15 @@
+package test3
+
+import "fmt"
+
+func main()  {
+	fmt.Println(465546)
+	fmt.Println(46546)
+	fmt.Println(46546)
+	fmt.Println(46566)
+	fmt.Println(46546)
+	fmt.Println(4656)
+	fmt.Println(46546)
+	fmt.Println(46546)
+}
+
