ad309c46b37cee6f0ec0f00ca983effd3bd8111b
moltenWood <84736566+moltenWood@users.noreply.github.com>
2021-09-09 22:49:45 +0800

    Update ClipBoard.kt
    upload

--- ClipBoard.kt
+++ ClipBoard.kt
@@ -1,1 +1,15 @@
+import java.awt.Toolkit
+import java.awt.datatransfer.Clipboard
+import java.awt.datatransfer.StringSelection
+import java.awt.datatransfer.Transferable
 
+object ClipBoard {
+    /**
+     * 将字符串复制到剪切板。
+     */
+    fun setSysClipboardText(writeMe: String?) {
+        val clip: Clipboard = Toolkit.getDefaultToolkit().getSystemClipboard()
+        val tText: Transferable = StringSelection(writeMe)
+        clip.setContents(tText, null)
+    }
+}
